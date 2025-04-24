be6594769a0bd878c853972235db308a95f05cf6
hyemin9999 <hmlim9999@gmail.com>
2025-04-24 17:10:33 +0900

    article 게시글 보기일때 게시글 수정 기능 추가

--- src/com/woori/BAM/Main.java
+++ src/com/woori/BAM/Main.java
@@ -1,33 +1,60 @@
 
 package com.woori.BAM;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
+	/**
+	 * 존재하지않는 게시글 입니다.
+	 */
 	public static String notAticleTxt = "존재하지않는 게시글 입니다.";
+	/**
+	 * 입력된 게시글 번호가 없습니다.
+	 */
 	public static String notAticleIndex = "입력된 게시글 번호가 없습니다.";
+
+	/**
+	 * 명령어 종류
+	 * 
+	 * @param list   = '게시글 목록'
+	 * @param write  = '게시글 작성'
+	 * @param detail = '게시글 보기'
+	 * @param edit   = '게시글 수정'
+	 * @param delete = '게시글 삭제'
+	 * @param exit   = '프로그램 종료'
+	 */
+	public static class Commands {
+		static String list = "article list";
+		static String write = "article write";
+		static String detail = "article detail";
+		static String edit = "article edit";
+		static String delete = "article delete";
+		static String exit = "exit";
+	}
 
 	public static void main(String[] args) {
 
 		// 게시글 배열 변수 선언
 		// ArrayList 자료구조로 인덱스를 가지고 객체주는 제한이 없음.
+		// List<Article> ==> 인터페이스, articles객체 변수명
+		// ArrayList<E> 는 implements(구현) List(interface)
 		List<Article> articles = new ArrayList<Article>();
 
-		int index = 0;
+		int id = 0;// 등록된 게시글 번호
+		int detailId = 0; // 상세보기 게시글 번호
 
-//		ItemCheck itemChk = new ItemCheck().setItemChk(false, false);
-
-		boolean listChk = false; // 게시글이 있는 게시글 목록인지 확인하는 용도
-		boolean viewChk = false; // 게시글 보기인지 확인 하는 용도
+		ItemCheck itemChk = new ItemCheck().setItemChk(false);
 
 		System.out.println("== 프로그램 시작 ==");
 
 		System.out.println("명령어 종류) article list : 게시글 목록 \narticle write : 게시글 작성 \n"
-				+ "article view \'번호\' : 입력한 번호의 게시글 내용 (\'\'제외, 숫자만 입력해주세요.)\n"
-				+ "article delete \'번호\' : 입력한 번호의 게시글 삭제 (\'\'제외, 숫자만 입력해주세요.) \n" + "exit : 종료");
+				+ "article detail \'번호\' : 입력한 번호의 게시글 상세보기 (\'\'제외, 숫자만 입력해주세요.)\n"
+				+ "article delete \'번호\' : 입력한 번호의 게시글 삭제 (\'\'제외, 숫자만 입력해주세요.) \n" + "exit : 종료\n"
+				+ "===============\n게시글 상세보기와 삭제는 게시글 목록 상태에서만 가능합니다.");
 
 		Scanner sc = new Scanner(System.in);
 
@@ -43,58 +70,70 @@ public class Main {
 			if (cmd.replace("	", "").replace(" ", "").length() == 0) {// 아무것도 입력 안했을때를 의미
 				System.out.println("명령어를 입력해 주세요");
 
-//				itemChk = itemChk.setItemChk(false, false);
+				itemChk = itemChk.setItemChk(false);
 
-				listChk = false; // 게시글 확인여부 초기화 false : 체크안함
 				continue;// 현재단계 중단 및 반복문 실행
 			}
 
-			if (listChk == true && cmd.contains("article view")) { // 게시글 항목을 보기
+			if (itemChk.viewChk == false) { // 게시글 보기 상태가 아니면 게시글 index초기화
+				detailId = 0;
+			}
 
-//				itemChk = itemChk.setItemChk(false, true);
-				listChk = false; // 게시글 보고 나서 게시글 목록으로 보내기 위한 체크
-				getItemPrint(cmd, articles);
+			if (itemChk.listChk == true && cmd.contains(Commands.detail.toString())) { // 게시글 항목을 보기
 
-			} else if (listChk == true && cmd.contains("article delete")) { // 개시글 삭제
-
-//				itemChk = itemChk.setItemChk(false, false);
-				listChk = false; // 게시글 보고 나서 게시글 목록으로 보내기 위한 체크
-				getItemPrint(cmd, articles);
-
-			} else if (cmd.equals("article list")) {// 게시글 목록
-				// 게시글 목록이 없을때를 먼저 체크하는게 좋다.
-				if (articles.size() == 0) { // 게시글 목록이 없을때를 의미
-					System.out.println("존재하는 게시글이 없습니다.");
-//					itemChk = itemChk.setItemChk(false, false);
-					listChk = false; // 게시글 확인여부 초기화 false : 체크안함
-				} else {
-					System.out.printf("%d개의 게시글이 있습니다.\n", articles.size());
-					System.out.println("번호	|	제목");
-					// 저장된 게시글 목록 출력 ==> 최신글이 상단에 위치하도록 역순
-					for (int i = articles.size() - 1; i >= 0; i--) {
-						System.out.printf("%d	|	%s\n", articles.get(i).index, articles.get(i).title);
-					}
-
-//					itemChk = itemChk.setItemChk(true, false);
-					listChk = true; // 게시글 목록 확인 여부 체크 true : 체크함
+				detailId = getId(cmd);
+				if (detailId != 0) {
+					getItemPrint(cmd, articles, itemChk);
 				}
 
-			} else if (cmd.equals("article write")) { // 글 생성
+			} else if (itemChk.listChk == true && cmd.contains(Commands.delete.toString())) { // 게시글 삭제
+
+				getItemPrint(cmd, articles, itemChk);
+
+			} else if (itemChk.viewChk == true && cmd.contains(Commands.edit.toString())) { // 게시글 수정 지금 선택된 게시글의 번호 필요
+
+				// 다시 제목 내용 입력받아서
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
-				Article aitem = new Article(++index, title, body); // 새성자 오버로딩을 통해 각 값을 생성자에 넘김
+				// 해당 게시글 번호 게시글에 수정하고 update날짜 현재날짜로 수정
+				Article aitem = getItem(articles, detailId - 1);
+				aitem.setArticle(title, body);
+
+				System.out.printf("%d번글이 수정되었습니다.\n", detailId);
+
+			} else if (cmd.equals(Commands.list.toString())) {// 게시글 목록
+				// 게시글 목록이 없을때를 먼저 체크하는게 좋다.
+				if (articles.size() == 0) { // 게시글 목록이 없을때를 의미
+					System.out.println("존재하는 게시글이 없습니다.\n게시글 작성을 먼저 해주세요.");
+					itemChk = itemChk.setItemChk(false);
+				} else {
+					System.out.printf("%d개의 게시글이 있습니다.\n", articles.size());
+					System.out.println("번호	|	제목");
+					// 저장된 게시글 목록 출력 ==> 최신글이 상단에 위치하도록 역순
+					for (int i = articles.size() - 1; i >= 0; i--) {
+						System.out.printf("%d	|	%s\n", articles.get(i).id, articles.get(i).title);
+					}
+
+					itemChk = itemChk.setItemChk(true, false);// 게시글 목록 확인 여부 체크 true : 체크함
+				}
+
+			} else if (cmd.equals(Commands.write.toString())) { // 글 작성
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				Article aitem = new Article(++id, title, body); // 새성자 오버로딩을 통해 각 값을 생성자에 넘김
 				articles.add(aitem);
 
-				System.out.printf("%d번글이 생성되었습니다.\n", index);
-//				itemChk = itemChk.setItemChk(false, false);
-				listChk = false; // 게시글 확인여부 초기화 false : 체크안함
+				System.out.printf("%d번글이 생성되었습니다.\n", id);
+				itemChk = itemChk.setItemChk(false);// 게시글 확인여부 초기화 false : 체크안함
 			} else { // 아무거나 입력했을때를 의미
 				System.out.println("존재하지 않는 명령어 입니다.");
-//				itemChk = itemChk.setItemChk(false, false);
-				listChk = false; // 게시글 확인여부 초기화 false : 체크안함
+				itemChk = itemChk.setItemChk(false);// 게시글 확인여부 초기화 false : 체크안함
 			}
 		}
 
@@ -106,39 +145,30 @@ public class Main {
 	/**
 	 * 항목을 선택해서 출력하는 함수
 	 * 
-	 * @param cmd   = 입력받은 명령어
-	 * @param aList = 넘겨받은 게시글 목록
+	 * @param cmd      = 입력받은 명령어
+	 * @param articles = 넘겨받은 게시글 목록
+	 * @param _itemChk = 넘겨받은 게시글 목록/보기 여부
 	 */
-	public static void getItemPrint(String cmd, List<Article> articles) {
+	public static void getItemPrint(String cmd, List<Article> articles, ItemCheck _itemChk) {
 
 		int num = articles.size() + 1; // 입력받은 게시글 번호가 게시글 목록의 갯수보다 클때 게시글 존재 하지않음을 처리하기 위한 변수
+		_itemChk.setItemChk(true, false);
 
-		if (cmd.contains("article view")) { // 게시글 보기 (article view 게시글 번호) 형식
+		num = getId(cmd);
 
-			if (cmd.replace("article view", "").trim().length() != 0) {
-				num = Integer.parseInt(cmd.replace("article view", "").trim());
-			} else { // 번호입력이 안되어있을때
-				System.out.println(notAticleIndex);
-			}
-		} else if (cmd.contains("article delete")) { // 게시글 삭제 (article delete 게시글 번호) 형식
-			if (cmd.replace("article view", "").trim().length() != 0) {
-				num = Integer.parseInt(cmd.replace("article view", "").trim());
-			} else { // 번호입력이 안되어있을때
-				System.out.println(notAticleIndex);
-			}
-		}
+		if (num <= articles.size() && num != 0) { // 존재하는 게시글 여부 확인 : 입력받은 게시글번호가 게시글 목록보다 작거나 같은지 확인
+			// article은 articles.get() 을 통해 받은 객체를 재사용하기 위해 저장 용도로 사용됨
+			Article article = getItem(articles, num - 1); // 해당 인덱스의 게시글 가져오기. 인덱스는 입력된 번호 - 1
 
-		if (num <= articles.size()) { // 존재하는 게시글 여부 확인 : 입력받은 게시글번호가 게시글 목록보다 작거나 같은지 확인
-			// article은 articles.get()을 통해 받은 객체를 재사용하기위해
-			Article article = articles.get(num - 1); // 해당 인덱스의 게시글 가져오기. 인덱스는 입력된 번호 - 1
+			if (cmd.contains(Commands.detail.toString())) { // 게시글 보기일때
+				System.out.printf("번호 : %d\n", article.id);
+				System.out.printf("날짜 : %s\n", article.update);
+				System.out.printf("제목 : %s\n", article.title);
+				System.out.printf("내용 : %s\n", article.body);
 
-			if (cmd.contains("article view")) { // 게시글 보기일때
-				System.out.printf("%d번의 게시글입니다.\n", num);
+				_itemChk.setItemChk(false, true);
 
-				System.out.println("번호	|	제목	|	내용");
-				System.out.printf("%d	|	%s	|	\"%s\"\n", article.index, article.title, article.body);
-
-			} else if (cmd.contains("article delete")) { // 게시글 삭제일때
+			} else if (cmd.contains(Commands.delete.toString())) { // 게시글 삭제일때
 				articles.remove(num - 1);
 
 				System.out.printf("%d번의 게시글이 삭제되었습니다.\n", num);
@@ -148,26 +178,96 @@ public class Main {
 			System.out.println(notAticleTxt);
 		}
 	}
+
+	/**
+	 * 게시글 목록에서 id로 게시글 반환 함수
+	 * 
+	 * @param _articles = 게시글 목록
+	 * @param id        = 입력한 게시글 번호
+	 */
+	public static Article getItem(List<Article> _articles, int id) {
+
+		return _articles.get(id);
+	}
+
+	/**
+	 * 입력된 명령어에서 게시글 번호가 입력되었는지 체크후 번호 반환 반환값이 0이면 게시글이 없는것
+	 * 
+	 * @param cmd = 명령어
+	 */
+	public static int getId(String cmd) {
+		int id = 0;
+		if (cmd.contains(Commands.detail.toString())) { // 게시글 보기 (article view 게시글 번호) 형식
+			if (isNumberic(cmd.replace(Commands.detail.toString(), "").trim())) {
+				id = Integer.parseInt(cmd.replace(Commands.detail.toString(), "").trim());
+			} else { // 번호입력이 안되어있을때
+				System.out.println(notAticleIndex);
+			}
+		} else if (cmd.contains(Commands.delete.toString())) { // 게시글 삭제 (article delete 게시글 번호) 형식
+			if (isNumberic(cmd.replace(Commands.delete.toString(), "").trim())) {
+				id = Integer.parseInt(cmd.replace(Commands.delete.toString(), "").trim());
+			} else { // 번호입력이 안되어있을때
+				System.out.println(notAticleIndex);
+			}
+		}
+
+		return id;
+	}
+
+	/**
+	 * 입력된 게시글 번호가 숫자인지 체크하는 함수
+	 */
+	public static boolean isNumberic(String str) {
+		return str.matches("[+-]?\\d*(\\.\\d+)?");
+	}
 }
 
+/**
+ * 게시글 class
+ * 
+ * @param id    = 번호
+ * @param title = 제목
+ * @parem body = 내용
+ * @param date   = 등록날짜
+ * @param update = 수정날짜
+ */
 class Article {
-	int index = 0;
-	// 제목
+	int id = 0;
 	String title = null;
-	// 내용
 	String body = null;
+	String date = null;
+	String update = null;
 
 	/**
 	 * 게시글 생성자
 	 * 
-	 * @param _index = 번호
+	 * @param _id    = 번호
 	 * @param _title = 제목
 	 * @param _body  = 내용
 	 */
-	Article(int _index, String _title, String _body) {
-		index = _index;
+	Article(int _id, String _title, String _body) {
+		id = _id;
 		title = _title;
 		body = _body;
+
+		// 현재 날짜/시간
+		LocalDateTime now = LocalDateTime.now(); // 현재 날짜/시간 출력
+		date = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+		update = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+	}
+
+	/**
+	 * 게시글 수정하기위해 입려받은 제목과 내용을 재정의하고 수정날짜도 재정의한다.
+	 * 
+	 * @parma _title = 제목
+	 * @param _body = 내용
+	 */
+	public void setArticle(String _title, String _body) {
+		title = _title;
+		body = _body;
+
+		LocalDateTime now = LocalDateTime.now(); // 현재 날짜/시간 출력
+		update = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
 	}
 }
 
@@ -177,16 +277,24 @@ class Article {
  * @param listChk = '게시글 목록' 체크 여부
  * @param viewChk = '게시글 보기' 체크 여부
  */
-//class ItemCheck {
-//	boolean listChk = false; // 게시글 목록부분인지 체크하는 용도
-//	boolean viewChk = false; // 게시글 보기부분인지 체크하는 용도
-//
-//	public ItemCheck setItemChk(boolean _listChk, boolean _viewChk) {
-//		listChk = _listChk;
-//		viewChk = _viewChk;
-//
-//		System.out.println("게시글 목록 여부 : " + listChk + "\n게시글 보기 여부 : " + viewChk);
-//
-//		return this;
-//	}
-//}
+class ItemCheck {
+	boolean listChk = false; // 게시글 목록부분인지 체크하는 용도
+	boolean viewChk = false; // 게시글 보기부분인지 체크하는 용도
+
+	public ItemCheck setItemChk(boolean _listChk, boolean _viewChk) {
+		listChk = _listChk;
+		viewChk = _viewChk;
+
+		return this;
+	}
+
+	/**
+	 * listChk 과 viewChk이 동일한 값으로 설정하는 함수
+	 */
+	public ItemCheck setItemChk(boolean _Chk) {
+		listChk = _Chk;
+		viewChk = _Chk;
+
+		return this;
+	}
+}
